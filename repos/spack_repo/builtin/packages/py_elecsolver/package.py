3f61c0c7edf879757024b76832167dd6b4246e79
William PIAT <william.piat3@gmail.com>
2026-05-05 17:29:19 +0200

    add elecsolver + python mumps

---
+++ repos/spack_repo/builtin/packages/py_elecsolver/package.py
@@ -0,0 +1,32 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyElecsolver(PythonPackage):
+    """Formalizes electric systems as linear problems for temporal and frequency-domain studies."""
+
+    homepage = "https://github.com/williampiat3/ElecSolver"
+    pypi = "ElecSolver/elecsolver-2.0.1.tar.gz"
+
+    maintainers = ["williampiat3"]
+
+    version("2.0.1", sha256="126b02e90e01405109ddd52255a43015d5e0e634945c46baa13c6d41d0e4e05e")
+
+    with default_args(type="build"):
+         depends_on("py-setuptools")
+    # Python dependencies
+    with default_args(type=("run")):
+        depends_on("python@3.9:")
+        depends_on("py-numpy")
+        depends_on("py-scipy")
+        depends_on("py-networkx")
+
+    # Optional/test deps
+    depends_on("py-pytest", type="test")
---
+++ repos/spack_repo/builtin/packages/py_python_mumps/package.py
@@ -0,0 +1,65 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT
+
+import os
+
+from spack_repo.builtin.build_systems.meson import MesonBuilder
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyPythonMumps(PythonPackage):
+    """Python wrapper for the MUMPS solver (python_mumps)."""
+
+    homepage = "https://gitlab.kwant-project.org/kwant/python-mumps"
+    pypi = "python-mumps/python_mumps-0.0.6.tar.gz"
+
+    maintainers = ["williampiat3"]
+
+    version("0.0.6", sha256="58c33104f77c448e127e9e6da316f71dfb1a17719ecf022634669d513306b1fa")
+
+    variant("mpi", default=True, description="Whether to have MPI support on python-mumps or not")
+
+    # build dependencies
+    with default_args(type="build"):
+        depends_on("meson@1.8:")
+        depends_on("ninja")
+        depends_on("py-meson-python@0.18:")
+        depends_on("py-setuptools")
+        depends_on("py-setuptools-scm")
+        depends_on("py-cython@3.1.1:")
+        depends_on("pkgconfig")
+
+    # Python dependencies
+    with default_args(type=("build", "run")):
+        depends_on("python@3.10:")
+        depends_on("py-numpy@2:")
+        depends_on("py-mpi4py@4.1:4", when="+mpi")
+        depends_on("py-scipy@1.13:")
+
+    # Optional/test deps
+    depends_on("py-pytest", type="test")
+
+    # External solver
+    depends_on("xkblas+pkgconfig")
+    depends_on("mumps+float+complex+double+metis+scotch+pkgconfig+cuda", when="+mpi")
+    depends_on("mumps~mpi+float+complex+double+metis+scotch+pkgconfig+cuda", when="~mpi")
+
+    patch("patch_xkblas.patch")
+    patch("patch_meson_build.patch")
+
+    @run_before("install")
+    def setup_meson(self) -> None:
+        """Running meson setup before building the package"""
+        # Building with meson
+        options = []
+        if self.spec["meson"].satisfies("@0.64:"):
+            options.append("setup")
+        options.append(os.path.abspath(self.stage.source_path))
+        options += MesonBuilder.std_args(self)
+        builddir = "builddir"
+        options.append(builddir + "/")
+        with working_dir(join_path(self.build_directory, builddir), create=True):
+            self.module.meson(*options)
