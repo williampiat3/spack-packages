0d18f7baccdafaed9f83a6a25173ec46b23dd7c4
williampiat3 <32038421+williampiat3@users.noreply.github.com>
2026-05-21 11:06:50 +0200

    Update package.py

--- repos/spack_repo/builtin/packages/py_plaid/package.py
+++ repos/spack_repo/builtin/packages/py_plaid/package.py
@@ -13,7 +13,6 @@ class PyPlaid(PythonPackage):
 
     maintainers("williampiat3", "casenave", "bstaber")
 
-
     license("MIT", checked_by="casenave")
 
     version("0.1.15", sha256="e596ee155804da31793af0ee8f0e93c5fe629e246cbdca87dcae741a1e1f1205")
