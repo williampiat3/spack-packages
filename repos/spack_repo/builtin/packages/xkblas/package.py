03d411723966d7df47ebcc3eb3a3501ac84972a1
williampiat3 <32038421+williampiat3@users.noreply.github.com>
2026-05-26 09:12:30 +0200

    Extend Xkblas class to support CUDA and ROCm

--- repos/spack_repo/builtin/packages/xkblas/package.py
+++ repos/spack_repo/builtin/packages/xkblas/package.py
@@ -3,11 +3,13 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+from spack_repo.builtin.build_systems.rocm import ROCmPackage
 
 from spack.package import *
 
 
-class Xkblas(CMakePackage):
+class Xkblas(CMakePackage,CudaPackage,ROCmPackage):
     """
     XKBlas is a BLAS library for multi-GPUs servers similar to CUBLASXt but with
     higher performances especially when matrix dimensions becomes smaller.
@@ -19,13 +21,11 @@ class Xkblas(CMakePackage):
     maintainers("williampiat3")
 
     license("CeCILL-C", checked_by="Thierry Gautier")
-    version("0.6.1",sha256="e038315be4b5d71f25bf2f8bb10f6a7629f06af985a45c09fcfb0f8948565212")
+    version("0.6.1", sha256="e038315be4b5d71f25bf2f8bb10f6a7629f06af985a45c09fcfb0f8948565212")
     version("0.6.0", sha256="1e3d6a9f7a09f15cc76d46583843a69e769b68252791dc13460548c4b54212bf")
 
     variant("mkl", default=False, description="Build with MKL support")
     variant("openblas", default=True, description="Build with OpenBLAS support")
-    variant("cuda", default=True, description="Build with CUDA support")
-    variant("rocm", default=False, description="Build with ROCm support")
     ## Only for grace hopper GPUs
     variant("unified", default=False, description="Build with unified memory support")
     variant("pkgconfig", default=False, description="Add pkgconfigs")
@@ -38,7 +38,8 @@ class Xkblas(CMakePackage):
     ## Tested with CUDA 8,9, 11, 12.0.0 and 13.0.1 without success
     ## Only CUDA 12.9.1 works with xkblas 0.6.0 within my limited testing.
     depends_on("cuda@12.9.1", when="+cuda")
-    depends_on("rocm", when="+rocm")
+    ## TODO: test on a AMD GPU.
+    depends_on("hip", when="+rocm")
 
     conflicts(
         "+mkl",
@@ -64,7 +65,7 @@ class Xkblas(CMakePackage):
                 )
             )
             args.append(self.define("BLAS_INCLUDE_DIRS", self.spec["openblas"].prefix.include))
-        args.append(self.define("KAAPI_BUILD_TESTING", True))
+        # args.append(self.define("KAAPI_BUILD_TESTING", True))
         return args
 
     @run_after("install", when="+pkgconfig")
